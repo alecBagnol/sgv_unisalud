32ce3be79bdc77760b6bb5fd18c872e52c357c77
Alec Bagnol <alphagrim90@gmail.com>
2021-04-23 16:53:51 -0500

    update database creation modules

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import modulos.creacion_db as db_create
 
 # comentar cuando la tabla ya sea creada
+db_create.crear_tabla_cities()
+db_create.crear_tabla_vaccines()
+db_create.crear_tabla_vaccine_lot()
+db_create.crear_tabla_vaccination_plan()
 db_create.crear_tabla_afiliados()
+db_create.crear_tabla_vaccination_schedule()
 
--- modulos/creacion_db.py
+++ modulos/creacion_db.py
@@ -1,38 +1,112 @@
 import sqlite3
 
-# def crear_tabla_vaccinated_status():
-#     con = sqlite3.connect('./db/db_unisalud.db')
-#     cursor = con.cursor()
-#     cursor.execute("""CREATE TABLE VaccinatedStatus (
-#         UserId DATATYPE integer NOT NULL PRIMARY KEY,
-#         FirstName DATATYPE text,
-#         LastName DATATYPE text,
-#         Address DATATYPE text,
-#         PhoneNum DATATYPE integer,
-#         Email DATATYPE text,
-#         City DATATYPE text,
-#         BirthDate DATATYPE integer,
-#         AffilitionDate DATATYPE integer,
-#         DisaffiliationDate DATATYPE integer,
-#         VaccinatedStatus DATATYPE text,
-#     )""")
+def crear_tabla_cities():
+    con = sqlite3.connect('./db/db_unisalud.db')
+    cursor = con.cursor()
+
+    cursor.execute("""CREATE TABLE cities (
+        id integer NOT NULL PRIMARY KEY,
+        city_name text
+    )""")
+
+    con.commit()
+    con.close()
+
+def crear_tabla_vaccines():
+    con = sqlite3.connect('./db/db_unisalud.db')
+    cursor = con.cursor()
+    
+    cursor.execute("""CREATE TABLE vaccines (
+        id integer NOT NULL PRIMARY KEY,
+        lab text,
+        type text,
+        doses text,
+        storege_temp integer,
+        expiration_date integer,
+        effectiveness real,
+        protection_time integer
+    )""")
+
+    con.commit()
+    con.close()
+
+def crear_tabla_vaccine_lot():
+    con = sqlite3.connect('./db/db_unisalud.db')
+    cursor = con.cursor()
+    
+    cursor.execute("""CREATE TABLE vaccine_lot (
+        id integer NOT NULL PRIMARY KEY,
+        vaccine_id integer,
+        quantity integer,
+        doses_used_id integer,
+        image_url text,
+        FOREIGN KEY(vaccine_id)
+            REFERENCES vaccines (id)
+    )""")
+
+    con.commit()
+    con.close()
+
+def crear_tabla_vaccination_plan():
+    con = sqlite3.connect('./db/db_unisalud.db')
+    cursor = con.cursor()
+    
+    cursor.execute("""CREATE TABLE vaccination_plan (
+        id integer NOT NULL PRIMARY KEY,
+        max_age integer,
+        min_age integer,
+        start_date integer,
+        end_date integer
+    )""")
+
+    con.commit()
+    con.close()
 
 def crear_tabla_afiliados():
     con = sqlite3.connect('./db/db_unisalud.db')
     cursor = con.cursor()
 
-    cursor.execute("""CREATE TABLE afiliados (
-        UserId DATATYPE integer NOT NULL PRIMARY KEY,
-        FirstName DATATYPE text,
-        LastName DATATYPE text,
-        Address DATATYPE text,
-        PhoneNum DATATYPE integer,
-        Email DATATYPE text,
-        City DATATYPE text,
-        BirthDate DATATYPE integer,
-        AffilitionDate DATATYPE integer,
-        DisaffiliationDate DATATYPE integer,
-        VaccinatedStatus DATATYPE text,
+    cursor.execute("""CREATE TABLE affiliates (
+        id integer NOT NULL,
+        first_name text,
+        last_name text,
+        address text,
+        phone integer,
+        email text,
+        city_id integer,
+        birth_date integer,
+        opt_in_date integer,
+        opt_out_date integer,
+        doses_taken integer,
+        vaccination_plan_id integer,
+        PRIMARY KEY(id),
+        FOREIGN KEY(vaccination_plan_id) 
+            REFERENCES vaccination_plan (id),
+        FOREIGN KEY(city_id)
+            REFERENCES cities (id)
+    )""")
+
+    con.commit()
+    con.close()
+
+def crear_tabla_vaccination_schedule():
+    con = sqlite3.connect('./db/db_unisalud.db')
+    cursor = con.cursor()
+    
+    cursor.execute("""CREATE TABLE vaccination_schedule (
+        vaccination_plan_id integer,
+        city_id integer,
+        vaccine_lot_id integer,
+        affiliate_id integer,
+        PRIMARY KEY(vaccination_plan_id, affiliate_id),
+        FOREIGN KEY(vaccination_plan_id)
+            REFERENCES vaccination_plan (id),
+        FOREIGN KEY(affiliate_id)
+            REFERENCES affiliates (id),
+        FOREIGN KEY(vaccine_lot_id)
+            REFERENCES vaccine_lot (id),
+        FOREIGN KEY(city_id)
+            REFERENCES cities (id)
     )""")
 
     con.commit()
