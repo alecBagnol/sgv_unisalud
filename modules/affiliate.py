9c6c6624cff2f88628a98a264e3b5af42bc023a1
Alec Bagnol <alphagrim90@gmail.com>
2021-04-29 00:19:43 -0500

    update vaccination status

--- modules/affiliate.py
+++ modules/affiliate.py
@@ -1,9 +1,10 @@
 import sqlite3
-from contextlib import closing
 import sys
 from modules.create_connect import create_or_connect as db_link
-# from modules.interface import refresh_console, exit_interface, print_menu, main_menu
 from modules.utils import dict_factory
+from modules.vaccine_lot import use_vaccine, find_lot
+from contextlib import closing
+from datetime  import datetime
 import time
 import re
 
@@ -25,21 +26,21 @@ def add(
         with db_link() as con:
             with closing(con.cursor()) as cur:
                 cur.execute("INSERT INTO affiliate VALUES (?,?,?,?,?,?,?,?,?,?,?)",(
-                affiliate_id,
-                first_name,
-                last_name,
-                address,
-                phone,
-                email,
-                city,
-                birth_date,
-                affiliation_date,
-                vaccinated,
-                disaffiliation_date
-            ))
-    except sqlite3.IntegrityError as err:
-        print(f"No se pudo agregar a {first_name}, su número de id ya está en la base de datos.")
-
+                    affiliate_id,
+                    first_name,
+                    last_name,
+                    address,
+                    phone,
+                    email,
+                    city,
+                    birth_date,
+                    affiliation_date,
+                    vaccinated,
+                    disaffiliation_date
+                ))
+                return True
+    except sqlite3.IntegrityError:
+        return False
 
 def find(affiliate_id):
     try:
@@ -47,31 +48,67 @@ def find(affiliate_id):
             con.row_factory = dict_factory
             with closing(con.cursor()) as cur:
                 cur.execute("SELECT * from affiliate WHERE affiliate_id = (?)",(affiliate_id,))
-                items = cur.fetchall()
+                items = cur.fetchone()
                 return items
     
-    except sqlite3.IntegrityError as err:
-        print(f"No se pudo encontrar al usuario con Id: {affiliate_id}.")
-
+    except sqlite3.IntegrityError:
+        return None
 
 def disaffiliate(affiliate_id, date):
     try:
         with db_link() as con:
             with closing(con.cursor()) as cur:
                 cur.execute("UPDATE affiliate SET disaffiliation_date = (?), affiliation_date = NULL WHERE affiliate_id = (?)",(date,affiliate_id,))
-
-    except sqlite3.IntegrityError as err:
-        print(f"No se pudo desafiliar al usuario con Id: {affiliate_id}.")
-
+                return True
+    except sqlite3.IntegrityError:
+        return False
 
 def affiliate(affiliate_id, date):
     try:
         with db_link() as con:
             with closing(con.cursor()) as cur:
                 cur.execute("UPDATE affiliate SET affiliation_date = (?), disaffiliation_date = NULL WHERE affiliate_id = (?)",(date,affiliate_id,))
-    except sqlite3.IntegrityError as err:
-        print(f"No se pudo renovar afiliacion del usuario con Id: {affiliate_id}.")
+                return True
+    except sqlite3.IntegrityError:
+        return False
 
+def vaccinate(affiliate_id):
+    try:
+        vaccinated = find(affiliate_id)['vaccinated']
+        now = datetime.now()
+        if not vaccinated:
+            items = get_vaccination_schedule(affiliate_id)
+            date_obj = datetime.fromtimestamp(items['date_time']).date()
+            if date_obj == datetime.datetime.now().date():
+                lot_item = find_lot(items['vaccine_lot_id'])
+                use_vaccine(items['vaccine_lot_id'], lot_item['amount'], lot_item['used_amount'])
+                update_status(affiliate_id, True)
+                return True
+        else:
+            return False
+
+    except sqlite3.IntegrityError:     
+        return None
+
+def get_vaccination_schedule(affiliate_id):
+    try:
+        with db_link() as con:
+            con.row_factory = dict_factory
+            with closing(con.cursor()) as cur:
+                cur.execute("SELECT * from VaccinationSchedule WHERE affiliate_id = (?)",(affiliate_id,))
+                items = cur.fetchone()
+                return items
+    except sqlite3.IntegrityError:     
+        return None
+
+def update_status(affiliate_id, status):
+    try:
+        with db_link() as con:
+            with closing(con.cursor()) as cur:
+                cur.execute("UPDATE affiliate SET vaccinated = (?) WHERE affiliate_id = (?)",(status,affiliate_id,))
+                return True
+    except sqlite3.IntegrityError:
+        return False
 
 
 
