c090e47be0f80918352768ef07a0ae217ce979d2
ArmMaVid <araalvarezmo@unal.edu.co>
2021-04-28 22:51:43 -0500

    updates vaccination_plan to return a dic: field (key) and corresponding value, instead of a tuple

--- modules/vaccination_plan.py
+++ modules/vaccination_plan.py
@@ -5,9 +5,7 @@ Created on Sat Apr 24 09:32:42 2021
 @author: Pipe Alvarez
 """
 
-import sqlite3
-from sqlite3 import Error
-from create_connect import create_or_connect as db_link
+import create_connect as db
 
 def vacplan_insert(id , minumum_age , maximum_age , start_date , end_date):
     """
@@ -20,7 +18,7 @@ def vacplan_insert(id , minumum_age , maximum_age , start_date , end_date):
         startdate: start date of the Vaccination Plan (int)
         enddate : end date of the Vaccination Plan (int)
     """
-    conn = db_link()
+    conn = db.create_or_connect()
     cursor = conn.cursor()
     
     vacplan = id , minumum_age , maximum_age , start_date , end_date
@@ -31,21 +29,21 @@ def vacplan_insert(id , minumum_age , maximum_age , start_date , end_date):
     
 def vacplan_consult(idplan):
     """
-    Consults the infortmation of one instance of Vaccination Plan by idplan.
+    Consults the information of one instance of Vaccination Plan by idplan.
     
     Args:
         idplan: identification of the Vaccination Plan (int)
+        
+    Returns:
+        a dictionay with the information of one instance of Vaccination Plan: the name of the field (key) with its corresponding value
     """
-    try:
-        conn = db_link()
-        cursor = conn.cursor()
+    conn = db.create_or_connect()
+    cursor = conn.cursor()
         
-        cursor.execute("SELECT * from VaccinationPlan WHERE idplan = (?)",(id,))
-        records = cursor.fetchall()
-        for record in records:
-            print(record)
+    cursor.execute("SELECT * from VaccinationPlan WHERE idplan = (?)",(id,))
+    records = cursor.fetchall()
+    return {'idplan': records[0][0], 'minage': records[0][1], 'maxage': records[0][2], 'startdate': records[0][3], 
+            'enddate': records[0][4]}
         
-        conn.commit()
-        conn.close()
-    except IndexError:
-        print('Invalid id for Vaccination Plan')
+    conn.commit()
+    conn.close()
